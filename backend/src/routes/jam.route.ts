a6919c9b5f52e2d0e78f4f8f6ffdc99ab86035ce
akayou_adane <akuadane1205@gmail.com>
2025-06-17 19:09:23 -0400

    added multiple routes

---
+++ backend/src/routes/jam.route.ts
@@ -0,0 +1,157 @@
+import {Request, Response } from 'express';
+import {pool} from '../config/database';
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (_req: Request, res: Response)=>{
+    res.json({message: 'Hello World'});
+});
+
+router.get('/:jam_id', async (req: Request, res: Response)=>{
+    const {jam_id} = req.params;
+    const client = await pool.connect();
+    // TODO: get user_id from the request/ session
+    const user_id = 1;
+    try{
+        const result = await client.query(`
+            SELECT * 
+            FROM jam_user
+            WHERE jam_id = $1 AND user_id = $2
+        `,[jam_id, user_id]);
+        if(result.rows.length === 0){
+            res.status(404).json({error: 'Jam not found'});
+        }else{
+            
+            // get problems as json
+            const jam = await client.query(`
+                SELECT 
+                    jam.*,
+                    (
+                        SELECT json_agg(
+                            json_build_object(
+                                'topic', topic,
+                                'problems', problems
+                            )
+                        )
+                        FROM (
+                            SELECT 
+                                p.topic,
+                                json_agg(
+                                    json_build_object(
+                                        'problem_id', p.problem_id,
+                                        'name', p.name,
+                                        'difficulty', p.difficulty,
+                                        'url', p.url,
+                                        'tags', p.tags,
+                                        'topic_order', p.topic_order,
+                                        'solved_by', jp.solved_by
+                                    ) ORDER BY p.topic_order
+                                ) as problems
+                            FROM jam_problem jp
+                            JOIN problem p ON jp.problem_id = p.problem_id
+                            WHERE jp.jam_id = jam.jam_id
+                            GROUP BY p.topic
+                            ORDER BY MIN(p.topic_order)
+                        ) as topic_groups
+                    ) as sections
+                FROM jam
+                WHERE jam.jam_id = $1
+            `, [jam_id]);
+            res.status(200).json(jam.rows[0]);
+        }
+    }catch(err: any){
+        console.log(err);
+        res.status(500).json({error: err.message});
+    }finally{
+        client.release();
+    }
+
+});
+
+router.post('/createFromPlan', async (req: Request, res: Response)=>{
+    const {plan_id} = req.body;
+    // TODO: get user_id from the request/ session
+    const {user_id} = req.body;
+    const client = await pool.connect();
+    // transaction to insert jam_user and jam_problem
+    try{
+        await client.query('BEGIN');
+        const result = await client.query('INSERT INTO jam (plan_id, name,prob_goal_per_day,start_date,end_date,status,live_call) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING jam_id',[plan_id, req.body.name, req.body.prob_goal_per_day, req.body.start_date, req.body.end_date, req.body.status, req.body.live_call]);
+        await client.query('INSERT INTO jam_user (jam_id, user_id, is_admin) VALUES ($1, $2, $3)',[result.rows[0].jam_id, user_id, true]);
+        await client.query('INSERT INTO jam_problem (jam_id, problem_id) SELECT $1, problem_id FROM problem WHERE plan_id = $2',[result.rows[0].jam_id, plan_id]);
+        await client.query('COMMIT');
+        res.status(200).json({message: 'Jam created successfully', jam_id: result.rows[0].jam_id});
+    }catch(err: any){
+        await client.query('ROLLBACK');
+        res.status(500).json({error: err.message});
+    }finally{
+        client.release();
+    }
+});
+
+router.get('/ongoing', async (req: Request, res: Response)=>{
+    // status 1 is ongoing
+    // status 2 is completed
+    // status 0 is pending
+    const client = await pool.connect();
+    // TODO: get user_id from the request/ session
+    const user_id = 1;
+    client.query(`
+        SELECT jam.*, jam_user.is_admin
+        FROM jam 
+        LEFT JOIN jam_user ON jam.jam_id = jam_user.jam_id
+        where jam.status = 1 and jam_user.user_id = $1`
+        ,[user_id],(err: any,result: any)=>{
+        if(err){
+            res.status(500).json({error: err.message});
+        }else{
+            res.status(200).json(result.rows);
+        }
+    });
+});
+
+router.post('/adduser', async (req: Request, res: Response)=>{
+    const {jam_id, user_id} = req.body;
+    const client = await pool.connect();
+    try{
+        await client.query('INSERT INTO jam_user (jam_id, user_id) VALUES ($1, $2)',[jam_id, user_id]);
+        res.status(200).json({message: 'User added to jam successfully'});
+    }catch(err: any){
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.delete('/removeuser', async (req: Request, res: Response)=>{
+    const {jam_id, user_id} = req.body;
+    const client = await pool.connect();
+    try{
+        await client.query('DELETE FROM jam_user WHERE jam_id = $1 AND user_id = $2',[jam_id, user_id]);
+        res.status(200).json({message: 'User removed from jam successfully'});
+    }catch(err: any){
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.patch('/solved', async (req: Request, res: Response)=>{
+    const {jam_problem_id, user_id} = req.body;
+    const client = await pool.connect();
+    try{
+        await client.query("UPDATE jam_problem SET solved_by = COALESCE(solved_by, '{}')|| $1 WHERE jam_problem_id = $2",[user_id, jam_problem_id]);    
+        res.status(200).json({message: 'Problem solved successfully'});
+    }catch(err: any){   
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.patch('/unsolved', async (req: Request, res: Response)=>{
+    const {jam_problem_id, user_id} = req.body;
+    const client = await pool.connect();
+    try{
+        await client.query("UPDATE jam_problem SET solved_by = array_remove(solved_by, $1) WHERE jam_problem_id = $2",[user_id, jam_problem_id]);
+        res.status(200).json({message: 'Problem unsolved successfully'});
+    }catch(err: any){
+        res.status(500).json({error: err.message});
+    }
+});
+export {router as jamRouter};
