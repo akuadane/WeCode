29c8eb4f7c613410ba0be5b35e3333db8f19d9e3
natitedros <tedros.natnael@gmail.com>
2025-12-03 21:42:43 -0500

    optimized juser and jams query

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -5,12 +5,19 @@ export interface IJamReference {
   is_admin: boolean;
 }
 
+export interface IUserMetrics {
+  problems_solved: number;
+  problems_attempted: string;
+  solved_problem_ids: Types.ObjectId[];
+}
+
 export interface IUser extends Document {
   _id: Types.ObjectId;
   name: string;
   email?: string;
   created_at: Date;
   jams: IJamReference[];
+  metrics?: IUserMetrics;
 }
 
 const JamReferenceSchema = new Schema<IJamReference>({
